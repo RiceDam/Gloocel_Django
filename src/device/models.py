bfe51800c323b40ac5fb3314da69689598248f9f
Herbert Ma <38601017+herbertmaa@users.noreply.github.com>
2021-05-11 17:54:43 -0700

    Feature/device n door admin (#15)
    * added basic door api
    
    * updated admin side for doors, devices
    
    * merged with master
    
    * device admin looks betta

--- src/device/models.py
+++ src/device/models.py
@@ -13,7 +13,6 @@ class Device(models.Model):
   api_key = models.CharField(max_length=255, blank=True, null=True, editable=False)
   door = models.ForeignKey(Door, related_name='Door', null=True, on_delete=models.CASCADE)
 
-
   def save(self, *args, **kwargs):
     self.api_key = Device.key_default(self.device_name)
     super().save(*args, **kwargs)
--- src/door/models.py
+++ src/door/models.py
@@ -3,6 +3,9 @@ from location.models import Location
 
 # Create your models here.
 class Door(models.Model):
-    door_name = models.CharField(max_length = 50, null= True)
-    door_type = models.CharField(max_length = 30)
-    location = models.ForeignKey(Location, related_name='Location_Door', null=True, on_delete=models.CASCADE)
+ door_name = models.CharField(max_length = 50, null= True)
+ door_type = models.CharField(max_length = 30)
+ location = models.ForeignKey(Location, related_name='Location_Door', null=True, on_delete=models.CASCADE)
+
+ def __str__(self):
+  return self.door_name + ' | ' + str(self.location)
