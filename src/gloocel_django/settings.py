7fa14a4401b391ffe3e19c779a0c97692ba51eb5
Arka Roy <47661468+RoyArka@users.noreply.github.com>
2021-05-17 16:36:36 -0700

    Arka (#33)
    * replaced sqlite connection with dockerized postgress image
    
    * fixed merges and replaced db creds with env vars

--- src/gloocel_django/settings.py
+++ src/gloocel_django/settings.py
@@ -16,8 +16,6 @@ from dotenv import load_dotenv
 #Environment Variables
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'phonenumber_field',
     'pika',
+    'psycopg2',
     #own app components
     'accounts',
     'device',
@@ -91,9 +90,25 @@ WSGI_APPLICATION = 'gloocel_django.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    #POSTGRES
+        'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.getenv("NAME"),
+
+        'USER': os.getenv("USER"),
+
+        'PASSWORD': os.getenv("PASSWORD"),
+
+        'HOST': os.getenv("HOST"),
+
+        'PORT': os.getenv("PORT"),
     }
 }
 
